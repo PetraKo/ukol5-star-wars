6990b6f870caf3245e8ca6096f4b3b4cb3f2e4c8
Petra Koliskova <koliskova.petra@gmail.com>
2019-04-23 18:05:38 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -6,24 +6,28 @@ const apiUrl = "https://swapi.co/api/films/";
 
 fetch(`${apiUrl}`)
     .then(response => response.json())
-    .then(responseJson => displayStartWarsFilms(responseJson))
+    .then(responseJson => displayStarWarsFilms(responseJson))
     //  .catch(error => {
     //  console.error(`Došlo k chybě: ${error.message} ${error.stack}`);
     //  })
 ;
 
-function displayStartWarsFilms(data) {
+function displayStarWarsFilms(data) {
 
     data.results.map(movieFromApi => {
 
         let title = movieFromApi.title;
-        let episode = movieFromApi.episode_id;
+        let episode = movieFromApi.episode_id.sort((a,b) => a - b);
         let releaseDate = movieFromApi.release_date.slice(0,4);
         let director = movieFromApi.director;
     
         const movie = new Movie(episode, title, releaseDate, director);
-
         movie.renderHTML();
     });
 
+}
+
+function ascendingSort(data) {
+  data.episode.sort((a,b) => a - b);
+    // console.log(movie);  
 }
